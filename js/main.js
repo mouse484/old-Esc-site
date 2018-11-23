246c59bf17b40e5e66412fbea291b652b4e62883
unknown <=>
2018-11-23 14:59:24 +0900

    [update] 色々
    いじりすぎてわけわかめ

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-/*サイドバー動かすマン*/
 (function ($) {
     $(function () {
-
+        /*サイドバー動かすマン*/
         $('.sidenav').sidenav();
-
     }); // end of document ready
+
 })(jQuery); // end of jQuery name space
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+});
