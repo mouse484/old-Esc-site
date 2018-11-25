9e482b0b3d13f3d53e46e3077112cce1430a1960
mouse <=>
2018-11-25 17:57:57 +0900

    [update] jqery ~~~~~
    jqueryの奴を書き換えた

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-$(function () {
-    /*読み込み*/
-    //ヘッダー
+$(window).on("load", function () {
     $("#header").load("parts/header.html");
+    $("#main").load("contents/home.html");
     $("#footer").load("parts/footer.html");
 });
+
+$(function () {
+    $(".home").on("click", function() {
+        $("#main").load("contents/home.html")
+    });
+    $(".join").on("click", function () {
+        $("#main").load("contents/join.html")
+    });
+});
+
+
