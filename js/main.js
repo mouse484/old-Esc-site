15ec1d9e9a51592fe90431fa105b4e4bb49d2d7e
mouse <=>
2018-11-26 23:10:29 +0900

    [add] pushState
    URL問題の解消しようとしてる

--- js/main.js
+++ js/main.js
@@ -1,18 +1,45 @@
+function home() {
+    $("#main").load("contents/home.html");
+};
+function join() {
+    $("#main").load("contents/join.html");
+};
+function servers() {
+    $("#main").load("contents/servers.html");
+};
+
 $(window).on("load", function () {
     $("#header").load("parts/header.html");
-    $("#main").load("contents/home.html");
+    home();
     $("#footer").load("parts/footer.html");
 });
 
+$(window).on("popstate", function(e) {
+    let state = e.originalEvent.state;
+    console.log(state);
+    if (state === "home") {
+        home();
+    } else if (state === "join") {
+        join();
+    } else if (state === "servers") {
+        servers();
+    } else {
+        home();
+    }
+});
+
 $(function () {
-    $(".home").on("click", function() {
-        $("#main").load("contents/home.html")
+    $(".home").on("click", function () {
+        home();
+        history.pushState("home", "", "?home");
     });
     $(".join").on("click", function () {
-        $("#main").load("contents/join.html")
+        join();
+        history.pushState("join", "", "?join");
     });
     $(".servers").on("click", function () {
-        $("#main").load("contents/servers.html")
+        servers();
+        history.pushState("servers", "", "?servers");
     });
 });
 
