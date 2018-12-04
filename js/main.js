7eef9a961a9219ef41a83c58baa910ca645326f1
mouse <=>
2018-12-04 14:12:27 +0900

    [update] 色々変えてみようとしているです

--- js/main.js
+++ js/main.js
@@ -1,45 +1,17 @@
-function home() {
-    $("#main").load("contents/home.html");
-};
-function join() {
-    $("#main").load("contents/join.html");
-};
-function servers() {
-    $("#main").load("contents/servers.html");
-};
-
 $(window).on("load", function () {
     $("#header").load("parts/header.html");
-    home();
+    $("#main").load("page/home/index.html#main")
     $("#footer").load("parts/footer.html");
 });
 
-$(window).on("popstate", function(e) {
-    let state = e.originalEvent.state;
-    console.log(state);
-    if (state === "home") {
-        home();
-    } else if (state === "join") {
-        join();
-    } else if (state === "servers") {
-        servers();
-    } else {
-        home();
-    }
-});
-
 $(function () {
-    $(".home").on("click", function () {
-        home();
-        history.pushState("home", "", "?home");
-    });
     $(".join").on("click", function () {
-        join();
-        history.pushState("join", "", "?join");
+        $("#main").load("page/join/index.html#main");
+        history.pushState("join", "", "/page/join/");
     });
     $(".servers").on("click", function () {
-        servers();
-        history.pushState("servers", "", "?servers");
+        $("#main").load("page/servers/index.html#main");
+        history.pushState("servers", "", "/page/servers/");
     });
 });
 
