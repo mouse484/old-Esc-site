8580809db3b04d2447743bccbfbfff661065e0f3
mouse <=>
2018-12-04 15:22:35 +0900

    [fix] path youso
    要素のぱすは etc

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 $(window).on("load", () => {
     $("#header").load("parts/header.html");
-    $("#main").load("page/home/index.html#main")
+    $("#main").load("page/home/index.html #main")
     $("#footer").load("parts/footer.html");
 });
 
 $(function () {
+    $(".jome").on("click", () => {
+        $("#main").load("page/home/index.html #main");
+        history.pushState("home","","/page/join/")
+    })
     $(".join").on("click", function () {
-        $("#main").load("page/join/index.html#main");
+        $("#main").load("page/join/index.html #main");
         history.pushState("join", "", "/page/join/");
     });
     $(".servers").on("click", function () {
-        $("#main").load("page/servers/index.html#main");
+        $("#main").load("page/servers/index.html #main");
         history.pushState("servers", "", "/page/servers/");
     });
 });
